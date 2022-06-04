a217394d33bf33a54df48c8bf7f8f6989b9c9451
aayush <aayushmittalktl2003@gmail.com>
2022-06-04 18:05:03 +0530

    Commiting quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void swap(int arr[], int i, int j){
+                 arr[i] = arr[i]+arr[j];
+                 arr[j]= arr[i]-arr[j];
+                arr[i] = arr[i]- arr[j];
+            }
+
+int partition(int arr[], int l, int r){
+    int pivot=arr[r];
+    int i = l-1;
+    for(int j=l; j<r; j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i+1, r);
+    return i+1;
+}
+void quickSort(int arr[], int n, int m){
+    if(l<r){
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi-1);
+        quickSort(arr, pi+1, r);
+    }
+}
+int main(){
+    int arr[5]={5, 4, 3, 2,1};
+    quickSort(arr, 0, 4);
+    for(i=0; i<=4; i++){
+        cout<<arr[i]<<"\t";
+
+    }cout<<endl;
+    return 0;
+}
